e9fe33404025c560cf636ca4a6426a8e18005189
Wagner Leonardi <leonardiwagner@gmail.com>
2019-07-31 11:37:10 -0300

    add docs

--- client/server.js
+++ client/server.js
@@ -12,20 +12,20 @@ app.get('/long', ({ query }, res) => {
   console.log(`[http-client] long request #${query.id}`)
 
   blockEvent(2).then(() => {
-    console.log(`[http-client] long response #${query.id}`)
+    console.log(`[client] long response #${query.id}`)
     res.sendStatus(200)
   })
 })
 
 app.get('/short', ({ query }, res) => {
-  console.log(`[http-client] short request #${query.id}`)
+  console.log(`[client] short request #${query.id}`)
 
   blockEvent(0.2).then(() => {
-    console.log(`[http-client] short response #${query.id}`)
+    console.log(`[client] short response #${query.id}`)
     res.sendStatus(200)
   })
 })
 
 app.listen('3002', () => {
-  console.log('[http-client] listening at :3002')
+  console.log('[client] listening at :3002')
 })
